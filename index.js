91a7a81c7695ca56d31aef200337784d1617f4f2
Stephen Onyango <onyangosteve01@gmail.com>
2024-03-19 02:03:54 +0300

    Arithmetics completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// First Test 
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+//Second test
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+//Third test
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+
+//Fourth test
+let max = Math.max(1, 2, 3, 4, 5, 20);
